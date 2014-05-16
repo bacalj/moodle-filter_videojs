3af1fd9fab78514efce50426f7e319d976d94d47
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-05-16 09:03:14 -0400

    adds a lib with a class for videojs shortcodes

---
+++ lib.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Video JS Filter
+ *
+ * Replaces a shortcode with a Video JS player
+ *
+ * @package    filter_videojs
+ * @copyright  2014 onwards Kevin Wiliarty {@link http://kevinwiliarty.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Video JS shortcode class.
+ *
+ * @package    filter_videojs
+ * @copyright  2014 onwards Kevin Wiliarty {@link http://kevinwiliarty.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class filter_videojs_shortcode {
+
+    /**
+     * Parse a videojs shortcode
+     *
+     * @return object A shortcode object
+     */
+    public function __construct($shortcodestring) {
+    }
+}
