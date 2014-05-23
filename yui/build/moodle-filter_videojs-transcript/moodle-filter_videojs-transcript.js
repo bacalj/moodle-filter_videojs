1695d485b2b1caa7ab8009061371b1f0863a9882
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-05-23 16:41:10 -0400

    gets a minimal YUI module working

--- yui/build/moodle-filter_videojs-transcript/moodle-filter_videojs-transcript.js
+++ yui/build/moodle-filter_videojs-transcript/moodle-filter_videojs-transcript.js
@@ -5,22 +5,13 @@ YUI.add('moodle-filter_videojs-transcript', function (Y, NAME) {
 /*global M*/
 /*global Y*/
 
-var CSS = {
-IPA_CBLOCK  : 'qtype-ipa-cblock',
-IPA_PREVIEW : 'qtype-ipa-preview'
-},
-SELECTORS = {
-IPA_CBLOCK  : '.' + CSS.IPA_CBLOCK,
-IPA_PREVIEW : '.' + CSS.IPA_PREVIEW
-},
-NS;
+var VJS;
 
 M.filter_videojs = M.filter_videojs || {};
 VJS = M.filter_videojs.transcript = {};
 
-
-VJS.init = function (params) {
-    Y.one('#example').set('innerHTML', 'Example content');
+VJS.init = function () {
+    // Y.one('#example').set('innerHTML', 'Example content');
     alert("Whoo-hooo!");
 }
 
