7a228cf156810b97a652bee95d6ab122c4c4bb14
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-05-31 01:30:17 -0400

    gets closer on passing json

--- yui/build/moodle-filter_videojs-transcript/moodle-filter_videojs-transcript-debug.js
+++ yui/build/moodle-filter_videojs-transcript/moodle-filter_videojs-transcript-debug.js
@@ -10,7 +10,7 @@ var VJS;
 M.filter_videojs = M.filter_videojs || {};
 VJS = M.filter_videojs.transcript = {};
 
-VJS.init = function (params) {
+VJS.init = function (clips) {
 
 Y.Get.load(['http://eik.local/videojs/dist/video-js/video-js.css', 'http://eik.local/videojs/dist/video-js/video.js'], function (err) {
     if (err) {
@@ -20,7 +20,11 @@ Y.Get.load(['http://eik.local/videojs/dist/video-js/video-js.css', 'http://eik.l
 
     Y.log('CSS loaded successfully');
     videojs.options.flash.swf = "http://eik.local/videojs/dist/video-js/video-js.swf";
+
+    console.log(JSON.parse(clips));
+
 });
+
 // console.log(JSON.parse(params));
     // Y.one('#example').set('innerHTML', 'Example content');
     // alert("Whoo-hooo!");
