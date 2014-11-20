d8aa26c8a6cb5850e19dee181f14a6a50f8fa1a5
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-11-20 11:49:17 -0500

    comments and solves exception of identical shortcodes

--- filter.php
+++ filter.php
@@ -43,32 +43,43 @@ class filter_videojs extends moodle_text_filter {
      */
     public function filter($text, array $options = array()) {
 
+        global $COURSE;
+
         // We'll use the context id to guarantee unique id's for the videos. 
         // This is necessary because...
         // Filtering happens at the mod level, and there may be several per page.
+        // It is not sufficient to expect an array index to be unique.
         $contextid = $this->context->id;
 
         // Define the string to pluck out.
         $regex = '\[videojs\].*?\[\/videojs\]';
 
         // Find all the matches and store them in a $shortcodes array.
+        // $shortcodes will be an array of arrays.
+        // The first element of each child array is the raw content of a shortcode.
         preg_match_all("/$regex/sm", $text, $shortcodes, PREG_SET_ORDER);
 
         // Replace the shortcodes with HTML5 video markup.
-        $vos = array();
+        // The patterns are the original markup.
         $patterns = array();
+        // The replacements are the HTML5 markup.
         $replacements = array();
+        // Build two arrays, one with patterns, the other with replacements.
         foreach ($shortcodes as $key => $sc) {
-            // Build the markup using the $contextid and the $shortcodes array index for uniqueness.
+            // Build the HTML5 markup using the $contextid and the $shortcodes array index for uniqueness.
             $vo = new filter_videojs_video($sc[0], $contextid . "_" . $key);
             $patterns[$key] = $sc[0];
             $replacements[$key] = "\n" . $vo->get_html() . "\n";
-            // This array may be unneccessary. Consider removing it.
-            $vos[] = $vo;
         }
-        global $COURSE;
+
         // Do the swap.
-        $text = str_replace($patterns, $replacements, $text);
+        // We have to worry about duplicate shortcode patterns, unfortunately.
+        // The replacements must have unique id's even if the shortcodes are identical.
+        foreach ( $patterns as $key => $pattern ) {
+            $pos = strpos( $text, $pattern );
+            $len = strlen( $pattern );
+            $text = substr_replace( $text, $replacements[$key], $pos, $len );
+        }
         return $text;
     }
 }
